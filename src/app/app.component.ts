c9e5d9b96fc0b738a4164b33cd09d2df86451b85
Victor Guerra <victorbcguerra@gmail.com>
2017-11-08 10:48:02 -0200

    fixing the angular-cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Angular2TokenService } from "angular2-token";
+import { environment } from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private authToken: Angular2TokenService){
+    this.authToken.init(environment.token_auth_config);
+
+    // Test the connection
+    this.authToken.signIn({email: "example@example.com", password: "12345"}).subscribe(
+      res => {
+        console.log('auth response:', res);
+        console.log('auth response headers: ', res.headers.toJSON());
+        console.log('auth response body:', res.json());
+      },
+      err => {
+        console.log('auth error:', err);
+      }
+    )
+  }
 }
